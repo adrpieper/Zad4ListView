5fad838356ca8f8b604ce9f6a9db0a27b04d024d
Adrian <adrpieper@gmail.com>
2017-01-17 19:10:39 +0100

    Dodałem klasę Person

---
+++ app/src/main/java/com/sda/pieper/zad4listview/Person.java
@@ -0,0 +1,22 @@
+package com.sda.pieper.zad4listview;
+
+public class Person {
+    private String name;
+    private String surname;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
