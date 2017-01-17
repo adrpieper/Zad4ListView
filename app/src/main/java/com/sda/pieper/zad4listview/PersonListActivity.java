7f96fe82d9bebd30018e826ab75ba5e39c308947
Adrian <adrpieper@gmail.com>
2017-01-17 20:46:58 +0100

    Oprogramowałem formularz

--- app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
+++ app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -19,10 +20,25 @@ public class PersonListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_list);
         ListView listView = (ListView) findViewById(R.id.person_list);
+        final EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        final EditText surnameEditText = (EditText) findViewById(R.id.surname_edit_text);
+        Button addButton = (Button) findViewById(R.id.add_button);
+
         PersonProvider personProvider = new PersonProvider();
-        List<Person> persons = personProvider.getPersons();
-        PersonListAdapter adapter = new PersonListAdapter(persons);
+        final List<Person> persons = personProvider.getPersons();
+        final PersonListAdapter adapter = new PersonListAdapter(persons);
         listView.setAdapter(adapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Person person = new Person();
+                person.setName(nameEditText.getText().toString());
+                person.setSurname(surnameEditText.getText().toString());
+                persons.add(person);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     class PersonListAdapter extends BaseAdapter {
