19e2213265413a6145496121c14b83b6a6614388
Adrian <adrpieper@gmail.com>
2017-01-17 19:19:59 +0100

    Dodałem główną aktywność

---
+++ app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
@@ -0,0 +1,13 @@
+package com.sda.pieper.zad4listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PersonListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_list);
+    }
+}
