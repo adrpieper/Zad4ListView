656544af47c465045b9c0061ba652ae685d8cd27
Adrian <adrpieper@gmail.com>
2017-01-17 19:37:05 +0100

    Utworzyłem PersonListAdapter

--- app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
+++ app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
@@ -2,7 +2,12 @@ package com.sda.pieper.zad4listview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class PersonListActivity extends Activity {
 
@@ -11,5 +16,34 @@ public class PersonListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_list);
         ListView listView = (ListView) findViewById(R.id.person_list);
+        listView.setAdapter();
+    }
+
+    class PersonListAdapter extends BaseAdapter {
+        private List<Person> persons;
+
+        public PersonListAdapter(List<Person> persons) {
+            this.persons = persons;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
