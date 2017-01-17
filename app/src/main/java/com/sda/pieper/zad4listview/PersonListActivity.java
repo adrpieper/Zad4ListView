58c178e1936d0223f2794c39b7d7374850a6ebe0
Adrian <adrpieper@gmail.com>
2017-01-17 19:33:02 +0100

    Pobrałem referencję do ListView

--- app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
+++ app/src/main/java/com/sda/pieper/zad4listview/PersonListActivity.java
@@ -2,6 +2,7 @@ package com.sda.pieper.zad4listview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class PersonListActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class PersonListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_list);
+        ListView listView = (ListView) findViewById(R.id.person_list);
     }
 }
